56036a9c1536fa105c5d7c2262368c168f21b464
Yanke Zhou <zhouyk_richard@outlook.com>
2025-08-09 19:38:10 +0800

    add inference agent

--- src/inference/agent.py
+++ src/inference/agent.py
@@ -84,7 +84,12 @@ class InferenceAgent(nn.Module):
             tokenizer (BaseTokenizer): the inner tokenizer, which supports the common APIs of `BaseTokenizer`
         """
         super().__init__()
-        raise NotImplementedError("TODO: Assignment5 - Task2")
+        self.config = config
+        self.model = model.eval().to(config.device)
+        self.tokenizer = tokenizer
+        
+        self._system_prompt = PromptTemplate()
+        self._context_prompt = PromptTemplate()
         
     def set_prompt(
         self,
@@ -97,7 +102,12 @@ class InferenceAgent(nn.Module):
             prompt_template (PromptTemplate): the prompt template
             prompt_type (PromptType): the prompt type
         """
-        raise NotImplementedError("TODO: Assignment5 - Task2")
+        if prompt_type == PromptType.SYSTEM:
+            self._system_prompt = prompt_template
+        elif prompt_type == PromptType.CONTEXT:
+            self._context_prompt = prompt_template
+        else:
+            raise ValueError(f"Invalid prompt type: {prompt_type}, prompt_type should be either SYSTEM or CONTEXT")
             
     def get_prompt(
         self,
@@ -111,7 +121,12 @@ class InferenceAgent(nn.Module):
         Returns:
             PromptTemplate: the prompt template
         """
-        raise NotImplementedError("TODO: Assignment5 - Task2")
+        if prompt_type == PromptType.SYSTEM:
+            return self._system_prompt
+        elif prompt_type == PromptType.CONTEXT:
+            return self._context_prompt
+        else:
+            raise ValueError(f"Invalid prompt type: {prompt_type}, prompt_type should be either SYSTEM or CONTEXT")
     
     @torch.no_grad()
     def forward(
@@ -135,8 +150,161 @@ class InferenceAgent(nn.Module):
             NOTE: to simplify, we do not use early stopping strategy since the stopping point for each response might vary, \
                 thus the length of the latent token ids for each response is ensured to be `max_new_tokens`
         """
-        raise NotImplementedError("TODO: Assignment5 - Task2")
+        
+        system_prompt = self._system_prompt(**kwargs)
+        context_prompt = self._context_prompt(**kwargs)
+        # prepare full prompts:
+        query = convert_to_list(query)
+        full_prompts = [system_prompt + context_prompt + q for q in query]
+        # tokenize the full prompts:
+        input_ids = self.tokenizer.encode(full_prompts).to(self.config.device)
+        # trunction:
+        if self.config.truncate_length is not None:
+            input_ids = self.__truncate_input_ids(input_ids, self.config.truncate_length, self.config.truncate_side)
+        # padding:
+        input_ids = self.__pad_input_ids(input_ids, self.config.pad_to_multiple_of, self.config.padding_side) 
+        # generate the responses:
+        responses: List[str] = self.generate(input_ids)
+        
+        return [
+            {
+                PromptType.SYSTEM: system_prompt,
+                PromptType.CONTEXT: context_prompt,
+                PromptType.QUERY: q,
+                PromptType.RESPONSE: response,
+                PromptType.PROMPT: full_prompt,
+                PromptType.ALL: full_prompts + response,
+            }
+            for q, full_prompt, response in zip(query, full_prompts, responses)
+        ]
+        
+    def generate(
+        self,
+        input_ids: List[torch.LongTensor],
+    ) -> List[str]:
+        """Generate the responses
+        """
+        batch_size = len(input_ids)
+        generation_ids = [[] for _ in range(batch_size)]
+        # reset kv cache
+        self.model.reset_kv_cache()
+        # generation loop:
+        for _ in range(self.config.max_new_tokens):
+            input_ids = torch.stack(
+                [input_id for input_id in input_ids],
+                dim=0,
+            ).to(self.config.device)
+            probs = self.model(input_ids=input_ids, temperature=self.config.temperature)
+            # the shape of probs is (batch_size, vocab_size)
+            sample_ids: List[torch.LongTensor] = []
+            if self.config.decode_strategy == DecodeStrategy.GREEDY:
+                sample_ids = self.__greedy_decode(probs)
+            elif self.config.decode_strategy == DecodeStrategy.SAMPLING:
+                sample_ids = self.__sampling_decode(probs)
+            else:
+                raise ValueError(f"Invalid decode strategy: {self.config.decode_strategy}, decode_strategy should be either GREEDY or SAMPLING")
+            for idx, sample_id in enumerate(sample_ids):
+                generation_ids[idx].append(sample_id)
+            
+            # catenate the sample ids to the input ids:
+            for ibx in input_ids.shape[0]:
+                input_ids[ibx] = torch.cat(
+                    (
+                        input_ids[ibx],
+                        sample_ids[ibx]
+                    ),
+                    dim=0
+                )
+        
+        # concatenate the generation_ids along the first dimension to convert
+        # from List[List[torch.LongTensor]] to List[torch.LongTensor]
+        concatenated_generation_ids = []
+        for batch_generation_ids in generation_ids:
+            concatenated_ids = torch.cat(batch_generation_ids, dim=0)
+            concatenated_generation_ids.append(concatenated_ids)
+        
+        # decode the concatenated generation ids to strings
+        return self.tokenizer.decode(concatenated_generation_ids)
     
+    
+    def __truncate_input_ids(
+        self, 
+        input_ids: List[torch.LongTensor],
+        truncate_length: int,
+        truncate_side: TruncateSide,
+    ) -> List[torch.LongTensor]:
+        """Truncate the input ids
+        """
+        for idx, input_id in enumerate(input_ids):
+            if truncate_side == TruncateSide.LEFT:
+                input_ids[idx] = input_id[-truncate_length:]
+            elif truncate_side == TruncateSide.RIGHT:
+                input_ids[idx] = input_id[:truncate_length]
+            elif truncate_side == TruncateSide.MIDDLE:
+                input_ids[idx] = torch.cat(
+                    (
+                        input_ids[idx][:truncate_length // 2],
+                        input_ids[idx][-truncate_length // 2:]
+                    ),
+                    dim=0
+                )
+            else:
+                raise ValueError(f"Invalid truncate side: {truncate_side}, truncate_side should be either LEFT, RIGHT, or MIDDLE")
+        return input_ids
+    
+    
+    def __pad_input_ids(
+        self,
+        input_ids: List[torch.LongTensor],
+        pad_to_multiple_of: int,
+        pad_side: PaddingSide,
+    ) -> List[torch.LongTensor]:
+        """Pad the input ids
+        """
+        max_input_length = max(input_id.shape[0] for input_id in input_ids)
+        # ensure the padded length is a multiple of `pad_to_multiple_of`:
+        padding_length = (max_input_length + pad_to_multiple_of - 1) // pad_to_multiple_of * pad_to_multiple_of
+        for idx, input_id in enumerate(input_ids):
+            to_be_padded = padding_length - input_id.shape[0]
+            if pad_side == PaddingSide.LEFT:
+                input_ids[idx] = F.pad(
+                    input_id,
+                    (to_be_padded, 0),
+                    value=self.tokenizer.bos_id
+                )
+            elif pad_side == PaddingSide.RIGHT:
+                input_ids[idx] = F.pad(
+                    input_id,
+                    (0, to_be_padded),
+                    value=self.tokenizer.eos_id
+                )
+            else:
+                raise ValueError(f"Invalid pad side: {pad_side}, pad_side should be either LEFT or RIGHT")
+            
+        return input_ids
+        
+    
+    def __greedy_decode(
+        self, 
+        probs: torch.Tensor,
+    ) -> List[torch.LongTensor]:
+        """Greedy decode the input ids
+        """
+        selected_ids = []
+        for prob in probs:
+            # the shape of prob is (batch_size, vocab_size)
+            select_id = torch.argmax(prob, dim=-1)
+            selected_ids.append(select_id)
+        return selected_ids
+    
+    def __sampling_decode(
+        self,
+        probs: torch.Tensor,
+    ) -> torch.LongTensor:
+        """Sampling decode the input ids
+        """
+        pass
+     
     @staticmethod
     def load_generation_config(
         config_file: str, 
--- src/modeling/prompt.py
+++ src/modeling/prompt.py
@@ -49,17 +49,31 @@ class PromptTemplate(nn.Module):
             template_str (str): the template string with the format: "....{key1}...{key2}..."
         """
         super().__init__()
-        raise NotImplementedError("TODO: Assignment5 - Task2")
-    
+        self.template_str = template_str
+        self.default_dict = {}
+        
+         
     def keys(self) -> Dict[str, Optional[str]]:
         """Get the keys with its default values of the prompt template as a dictionary
         NOTE: if any key has not been set with default value, then use `None` as a placeholder
         """
-        raise NotImplementedError("TODO: Assignment5 - Task2")
-    
+        if len(self.default_dict) == 0:
+            template_keys = find_format_keys(self.template_str)
+            for key in template_keys:
+                self.default_dict[key] = None
+        return self.default_dict
+            
+         
     def set_default(self, **kwargs: Optional[Dict[str, str]]) -> None:
-        """Set the default values of the prompt template keys"""
-        raise NotImplementedError("TODO: Assignment5 - Task2")
+        """Set the default values of the prompt template keys
+        NOTE: ignore the keys in kwargs but not in the template string
+        """
+        template_keys = find_format_keys(self.template_str)
+        for key in template_keys:
+            if key not in kwargs:
+                self.default_dict[key] = None
+            else:
+                self.default_dict[key] = kwargs[key]
     
     def forward(self, **kwargs: Optional[Dict[str, str]]) -> str:
         """Set the prompt template keys with the given keyword argument to get the formatted prompt
@@ -67,5 +81,12 @@ class PromptTemplate(nn.Module):
             1. if certain prompt template key has not been set with its default value, then its corresponding kwarg should be provided
             2. if certain key in the kwargs is not found in the keys of the prompt template, just ignore it
         """
-        raise NotImplementedError("TODO: Assignment5 - Task2")
-
+        keys = self.keys()
+        template_dict = {**self.default_dict}
+        for key in kwargs:
+            if key in keys:
+                template_dict[key] = kwargs[key]
+        for item in template_dict.items():
+            if item[1] is None:
+                raise ValueError(f"Prompt template key {item[0]} has not been set with a valid value")
+        return self.template_str.format(**template_dict)
